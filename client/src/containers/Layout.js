cde184203fed92cd9a6ff71c29a8d691a42dc87a
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2016-05-10 11:04:15 -0400

    Add react-hot-reload

---
+++ client/src/containers/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <h1>This has been a long way! ;|</h1>
+      {children}
+    </div>
+  )
+}
