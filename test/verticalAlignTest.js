f2508e0cbce3414a2d5e49e34d07f6adaa3cd999
emmanuelJd <elisa.emmanuel.jd@gmail.com>
2017-02-19 12:01:38 +0100

    re-write some function

--- test/verticalAlignTest.js
+++ test/verticalAlignTest.js
@@ -4,14 +4,17 @@ var assert = require('assert');
 var plate = require('./_initialData');
 var checkVertical = require('../lib/checkVertical');
 
-describe('---- Check vertical align ----', function() {
+describe('---- Check vertical align ----', function () {
 
-    it('for 4 pieces to win, it should return true in win object', function() {
+    it('for 4 pieces to win, it should return true in win object', function () {
         let pieceToWin = 4,
             pieceToPlay = {
-                x: 0,
-                y: 1,
-                user: 1
+                "id": 1,
+                "x": 0,
+                "y": 1,
+                "user": 1,
+                "weight": 5,
+                "state": 0
             };
         let result = checkVertical(plate.Plate, pieceToWin, pieceToPlay);
 
@@ -20,18 +23,21 @@ describe('---- Check vertical align ----', function() {
         assert.equal(result.count, 5);
 
         let winPiece = result.winPiece;
-        for(let i = 0, length = winPiece.length; i < length;i++){
-          assert.equal(winPiece[i].x, 0);
-          assert.equal(winPiece[i].state, 3);
+        for (let i = 0, length = winPiece.length; i < length; i++) {
+            assert.equal(winPiece[i].x, 0);
+            assert.equal(winPiece[i].state, 3);
         }
     });
 
-    it('for 4 pieces to win, it should return false in win object', function() {
+    it('for 4 pieces to win, it should return false in win object', function () {
         let pieceToWin = 4,
             pieceToPlay = {
-                x: 1,
-                y: 3,
-                user: 2
+                "id": 1,
+                "x": 1,
+                "y": 3,
+                "user": 2,
+                "weight": 5,
+                "state": 0
             };
         let result = checkVertical(plate.Plate, pieceToWin, pieceToPlay);
         assert.equal(result.win, false);
